b976425aef2d8dea8b90eb6c4c1b3172cdd6633e
giladreti <gilad.reti@gmail.com>
2017-11-13 14:50:27 +0200

    organized stuff into public folder

--- app/index.js
+++ app/index.js
@@ -4,25 +4,7 @@ var http = require('http').Server(app);
 var port = process.env.PORT || 3000;
 const path = require('path');
 
-app.get('/', function (req, res) {
-    res.sendFile(path.resolve(__dirname + '/../index.html'));
-  });
-  
-  app.get('/favicon.ico', function (req, res) {
-    res.sendFile(path.resolve(__dirname + '/../favicon.ico'));
-  });
-  
-  app.get('/style.css', function (req, res) {
-    res.sendFile(path.resolve(__dirname + '/../style.css'));
-  });
-  
-  app.use('/networks', express.static(__dirname+'/../networks'));
-  
-  app.use('/assets', express.static(__dirname + '/../assets'));
-  
-  app.use('/messenger', express.static(__dirname + '/../messenger'));
-  
-  app.use('/battleship', express.static(__dirname + '/../Battleship'))
+app.use(express.static('public'))
   
   http.listen(port, function () {
     console.log('listening on *:' + port);
